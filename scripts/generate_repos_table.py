10f868840ca43022b06adc5415d53b21242785ab
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2021-04-12 13:47:09 +0200

    resolve #14

--- scripts/generate_repos_table.py
+++ scripts/generate_repos_table.py
@@ -2,45 +2,66 @@ import os
 import sys
 import yaml
 
+from util.markdown import comma_separated, md_link, md_table, NEWLINE
+from util.github import github_user_link, github_repo_link
 
-def to_md_table(repos):
-    columns = ["Name", "Owner", "Description", "Languages"]
-    header = "| " + " | ".join(columns) + " |"
 
-    seprator = "| " + " | ".join(map(lambda _: "---", columns)) + " |\n"
+def fill_repo_details(repo, dir, file):
+    repo['owner'] = os.path.basename(dir)
+    repo['slug'] = os.path.splitext(file)[0]
+    if 'maintainers' not in repo:
+        repo['maintainers'] = [repo['owner']]
 
-    rows = seprator.join(
-        [
-            "| "
-            + " | ".join(
-                [
-                    f"[{repo['name']}]({repo['repository']})",
-                    f"[{repo['owner']}](https://github.com/{repo['owner']})",
-                    repo["description"],
-                    ", ".join(repo["languages"]),
-                ]
-            )
-            for repo in repos
-        ]
-    )
 
-    return f"{header}\n{seprator}{rows}"
+def repos_table(repos):
+    return md_table({
+        'Name': lambda repo: github_repo_link(repo['name'], repo['owner'], repo['slug']),
+        'Description': lambda repo: repo['description'],
+        'Owner': lambda repo: github_user_link(repo['owner']),
+        'Maintainers': lambda repo: comma_separated(map(github_user_link, repo['maintainers'])),
+        'Languages': lambda repo: comma_separated(repo['languages']),
+    }, repos)
+
+
+def load_repos(event):
+    FOLDER = 'repos'
+    repos = []
+    for current_path, _, files in os.walk(os.path.join(event, FOLDER)):
+        for file in files:
+            with open(os.path.join(current_path, file)) as f:
+                repo = yaml.full_load(f)
+                fill_repo_details(repo, current_path, file)
+            repos.append(repo)
+
+    return repos
+
+
+def write_repos(event, table):
+    FILE = 'README.md'
+    START_MARK = '<!-- Repos Table -->'
+    END_MARK = '<!-- /Repos Table -->'
+
+    readme = os.path.join(event, FILE)
+    with open(readme, 'r') as f:
+        data = f.read()
+        pre = data.index(START_MARK) + len(START_MARK)
+        post = data.index(END_MARK)
+
+    with open(readme, 'w') as f:
+        f.write(data[:pre] + NEWLINE +  table + NEWLINE + data[post:])
 
 
 def main():
     if len(sys.argv) < 2 or not os.path.isdir(sys.argv[1]):
-        print("Invalid argument")
+        print('Invalid argument')
         exit(1)
 
-    repos = []
-    for current_path, _, files in os.walk(sys.argv[1]):
-        for file in files:
-            with open(os.path.join(current_path, file)) as f:
-                repo = yaml.full_load(f)
-            repos.append(repo)
-
-    print(to_md_table(repos))
+    event = sys.argv[1]
+    repos = load_repos(event)
+    table = repos_table(repos)
+    write_repos(event, table)
 
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
---
+++ scripts/util/markdown.py
@@ -0,0 +1,29 @@
+NEWLINE = '\n'
+
+def comma_separated(list):
+    return ', '.join(list)
+
+
+def md_link(title, link):
+    return f'[{title}]({link})'
+
+
+def md_table(meta, data):
+    WALL = ' | '
+    FLOOR = '---'
+
+    columns = meta.keys()
+    header = WALL + WALL.join(columns) + WALL
+    separator = WALL + WALL.join(map(lambda _: FLOOR, columns)) + WALL + NEWLINE
+
+    rows = map(
+        lambda entry: WALL + WALL.join(
+            map(
+                lambda col: meta[col](entry)
+                , columns
+            )
+        ) + WALL
+        , data
+    )
+
+    return header + NEWLINE + separator + NEWLINE.join(rows)
